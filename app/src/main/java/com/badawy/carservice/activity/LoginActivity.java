0e5e3e858a05740f55b07ee1386f916acb6f5a91
Mahmoud Badawy <m.badawy2011@gmail.com>
2019-11-10 20:22:19 +0200

    Created the initial activities and fragments

--- app/src/main/java/com/badawy/carservice/activity/LoginActivity.java
+++ app/src/main/java/com/badawy/carservice/activity/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.badawy.carservice;
+package com.badawy.carservice.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.badawy.carservice.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
---
+++ app/src/main/java/com/badawy/carservice/activity/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.badawy.carservice.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.badawy.carservice.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
