c210e22af06aa7612d1b3166ace6755ef52cfbe5
Mahmoud Badawy <m.badawy2011@gmail.com>
2019-11-16 14:36:16 +0200

    Added Fonts,logo,drawables,colors,styles Finished Splash layout

--- app/src/main/java/com/badawy/carservice/activity/SplashScreenActivity.java
+++ app/src/main/java/com/badawy/carservice/activity/SplashScreenActivity.java
@@ -5,26 +5,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 import com.badawy.carservice.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
-    private  static int SPLASH_TIME_OUT=10000;//DATA MEM MEANS 2SEC m
-
+    /**
+    * Created  by Ahmed Tarek 15/11/2019
+    * Modified by Mahmoud Badawy 16/11/2019
+    **/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        hideSystemUI();
+        setSplashScreen();
 
-        new Handler().postDelayed(new Runnable(){
+    }
+
+
+    private void setSplashScreen() {
+
+        // Time in MilliSeconds 1000 = 1 second
+        int SPLASH_TIME_OUT = 5000;
+
+
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void run(){
-                Intent i = new Intent(SplashScreenActivity.this, LoginActivity.class);//error is here
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, LoginActivity.class);
                 startActivity(i);
                 finish();
 
 
             }
-        },SPLASH_TIME_OUT);
+        }, SPLASH_TIME_OUT);
     }
+
+    private void hideSystemUI() {
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
 }
