23b55de1f8ef80e05357d1b97f32a3abbb02f584
Mahmoud Badawy <m.badawy2011@gmail.com>
2019-11-17 05:06:47 +0200

    Finished xml of registration,login activities

--- app/src/main/java/com/badawy/carservice/activity/LoginActivity.java
+++ app/src/main/java/com/badawy/carservice/activity/LoginActivity.java
@@ -2,15 +2,43 @@ package com.badawy.carservice.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.badawy.carservice.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView login_tv_signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        hideSystemUI();
+
+        login_tv_signUp = findViewById(R.id.login_tv_signUp);
+        login_tv_signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegistrationActivity.class));
+            }
+        });
     }
+    private void hideSystemUI() {
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
 }
--- app/src/main/java/com/badawy/carservice/activity/RegistrationActivity.java
+++ app/src/main/java/com/badawy/carservice/activity/RegistrationActivity.java
@@ -5,9 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import java.util.Scanner;
+import android.widget.TextView;
 
 
 import com.badawy.carservice.R;
@@ -15,32 +16,45 @@ import com.badawy.carservice.R;
 public class RegistrationActivity extends AppCompatActivity {
 
     //uploaded by ahmed tarek..................
-    private EditText editTextfirstname;
-    private EditText editTextlastname;
+    private EditText editTextusername;
+    private EditText editTextphone;
     private EditText editTextemail;
     private EditText editTextpassword;
     private EditText editTextconfirmpassword;
     private Button   buttoncreateaccount;
-    
+    private TextView registration_tv_signIn; // badawy
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registeration);
-        //first step
+        setContentView(R.layout.activity_registration);
+        hideSystemUI(); //@badawy
 
-        editTextfirstname=findViewById(R.id.registration_et_firstName);
-        editTextlastname=findViewById(R.id.registration_et_lastName);
+
+        //first step
+        editTextusername =findViewById(R.id.registration_et_username); // modified first name to username
         editTextemail=findViewById(R.id.registration_et_emailAddress);
-        editTextpassword=findViewById(R.id.registration_et_Password);
+        editTextpassword=findViewById(R.id.registration_et_password);
         editTextconfirmpassword=findViewById(R.id.registration_et_confirmPassword);
         buttoncreateaccount=findViewById(R.id.registration_btn_createAccount);
+        editTextphone =findViewById(R.id.registration_et_phone); //modified last name to phone @badawy
 
-        editTextfirstname.addTextChangedListener(LoginTextwatcher);
-        editTextlastname.addTextChangedListener(LoginTextwatcher);
+
+        registration_tv_signIn = findViewById(R.id.registration_tv_signIn);//badawy
+        registration_tv_signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+        editTextusername.addTextChangedListener(LoginTextwatcher);
+        editTextphone.addTextChangedListener(LoginTextwatcher); //modified last name to phone @badawy
         editTextemail.addTextChangedListener(LoginTextwatcher);
         editTextpassword.addTextChangedListener(LoginTextwatcher);
         editTextconfirmpassword.addTextChangedListener(LoginTextwatcher);
-       // buttoncreateaccount.addTextChangedListener();
+        //buttoncreateaccount.addTextChangedListener();
 
     }
     private TextWatcher LoginTextwatcher=new TextWatcher() {
@@ -52,13 +66,15 @@ public class RegistrationActivity extends AppCompatActivity {
         @Override
         public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
             //new variables
-            String fistnameinput=editTextfirstname.getText().toString().trim();
-            String lastnameinput=editTextlastname.getText().toString().trim();
+            String fistnameinput= editTextusername.getText().toString().trim();
             String emailinput=editTextemail.getText().toString().trim();
             String password=editTextpassword.getText().toString().trim();
             String confirmoasswordinput=editTextconfirmpassword.getText().toString().trim();
+            String phoneinput=editTextphone.getText().toString().trim(); //modified last name to phone @badawy
+
             //the function is here ahmed.......!!
-            buttoncreateaccount.setEnabled(!fistnameinput.isEmpty() && !lastnameinput.isEmpty() && !emailinput.isEmpty() && !password.isEmpty() && !confirmoasswordinput.isEmpty());
+            //modified  && !lastnameinput.isEmpty()  to phone @badawy
+            buttoncreateaccount.setEnabled(!fistnameinput.isEmpty() && !emailinput.isEmpty() && !password.isEmpty() && !confirmoasswordinput.isEmpty() && !phoneinput.isEmpty());
 
         }
 
@@ -66,5 +82,24 @@ public class RegistrationActivity extends AppCompatActivity {
         public void afterTextChanged(Editable editable) {
 
         }
-    };
+           };
+
+
+    private void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
 }
