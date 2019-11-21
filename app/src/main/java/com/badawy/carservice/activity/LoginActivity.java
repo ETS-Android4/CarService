eeda35b41ee3e5d62b5a68480d82fec7e9c54fcb
Alfred <Alfred.onsi@outlook.com>
2019-11-21 22:49:47 +0200

    Add facebook login featur

--- app/src/main/java/com/badawy/carservice/activity/LoginActivity.java
+++ app/src/main/java/com/badawy/carservice/activity/LoginActivity.java
@@ -3,6 +3,7 @@ package com.badawy.carservice.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,26 +13,42 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+
+
+
 import com.badawy.carservice.utils.MyValidation;
 import com.badawy.carservice.R;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText emailET, passwordET;
     private Button signInBtn;
-    private ImageView facebookIcon, googleIcon, twitterIcon;
+    private ImageView  facebookIcon, googleIcon, twitterIcon;
     private FirebaseAuth mAuth;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initializeUi();
-
+        //callbackManager to handle login responses
+        callbackManager = CallbackManager.Factory.create();
         // Initialize FireBase Auth
         mAuth = FirebaseAuth.getInstance();
 
@@ -74,12 +91,33 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-
+        //@alfred
         //Facebook Authentication
         facebookIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this, Arrays.asList("email","public_profile"));
 
+                LoginManager.getInstance().registerCallback(callbackManager,
+                        new FacebookCallback<LoginResult>() {
+                            @Override
+                            public void onSuccess(LoginResult loginResult) {
+                                // App code
+                                handleFacebookAccessToken(loginResult.getAccessToken());
+
+
+                            }
+
+                            @Override
+                            public void onCancel() {
+                                // App code
+                            }
+
+                            @Override
+                            public void onError(FacebookException exception) {
+                                // App code
+                            }
+                        });
             }
         });
 
@@ -101,9 +139,48 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        //to
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
 
     }
+    // onActivityResult method to pass the login results to the LoginManager via callbackManager.
 
+
+
+    //@alfred
+    //to get an access token for the signed-in user ,but it in firebase then auth
+    private void handleFacebookAccessToken(AccessToken token) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent goToLoginActivity = new Intent(LoginActivity.this, HomepageActivity.class);
+                            startActivity(goToLoginActivity);
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+    //@alfred
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
     private void initializeUi() {
 
         // hideSystemUI();
