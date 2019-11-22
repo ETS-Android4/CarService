1b66240744f2249db4af4c78345c26c802369e39
abotarek5530 <abotarek19985530575@gmail.com>
2019-11-22 04:01:29 +0200

    new modification with addcar activity page........

--- app/src/main/java/com/badawy/carservice/activity/LoginActivity.java
+++ app/src/main/java/com/badawy/carservice/activity/LoginActivity.java
@@ -132,7 +132,7 @@ public class LoginActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
 
-                        /*modified by ahmed tarek*/    Intent goToLoginActivity = new Intent(LoginActivity.this, /*HomepageActivity*/AddCarActivity.class);
+                        /*modified by ahmed tarek*/    Intent goToLoginActivity = new Intent(LoginActivity.this, HomepageActivity/*AddCarActivity*/.class);
                             startActivity(goToLoginActivity);
 
                         } else {
