d3916c654d980895f1ad6e7c81e4afca1b2d3794
Mahmoud Badawy <m.badawy2011@gmail.com>
2020-03-03 02:25:24 +0200

    Garage Ui,fixed some bugs in different layouts

--- app/src/main/java/com/badawy/carservice/fragment/NavSettingsFragment.java
+++ app/src/main/java/com/badawy/carservice/fragment/NavSettingsFragment.java
@@ -8,8 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.badawy.carservice.R;
+import com.badawy.carservice.activity.HomepageActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,7 +28,15 @@ public class NavSettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nav_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_nav_settings, container, false);
+        ImageView navMenuBtn = view.findViewById(R.id.settings_navMenuBtn);
+        navMenuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HomepageActivity.openDrawer();
+            }
+        });
+        return view;
     }
 
 }
