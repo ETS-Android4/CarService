d0f2df96bb68b84355f8cd277148e2f47ca150be
AhmedRabie01 <49840480+AhmedRabie01@users.noreply.github.com>
2020-03-17 21:57:30 +0200

    Call Us Button

--- app/src/main/java/com/badawy/carservice/fragment/AboutUsFragment.java
+++ app/src/main/java/com/badawy/carservice/fragment/AboutUsFragment.java
@@ -1,26 +1,45 @@
 package com.badawy.carservice.fragment;
-
-
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
+import android.text.AlteredCharSequence;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.badawy.carservice.R;
 import com.badawy.carservice.activity.HomepageActivity;
+
+import java.security.ProtectionDomain;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AboutUsFragment extends Fragment {
 
+    private static final int REQUEST_CALL = 1;
 
     public AboutUsFragment() {
         // Required empty public constructor
+
+
     }
 
 
@@ -28,15 +47,66 @@ public class AboutUsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         View view = inflater.inflate(R.layout.fragment_about_us, container, false);
         ImageView navMenuBtn = view.findViewById(R.id.aboutUs_navMenuBtn);
+        TextView AboutUs_phoneNumberDescription = view.findViewById(R.id.aboutUs_phoneNumberDescription);
+        Button Aboutus_bu_callus = view.findViewById(R.id.aboutus_bu_callus);
+        final String number=AboutUs_phoneNumberDescription.getText().toString().trim();
+        Aboutus_bu_callus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MakePhoneCall(number);
+            }
+        });
         navMenuBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 HomepageActivity.openDrawer();
+
             }
         });
         return view;
+
+    }
+    private void MakePhoneCall(String num) {
+
+        String number = num;
+        if (number.trim().length() > 0) {
+            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+
+            } else {
+                String dial = "tel:" + number;
+                startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+            }
+
+
+        }
+        else {
+            Toast.makeText(getActivity(), "Enter phone number", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        final TextView AboutUs_phoneNumberDescription=getView().findViewById(R.id.aboutUs_phoneNumberDescription);
+
+        String number = AboutUs_phoneNumberDescription.getText().toString().trim();
+
+        if (requestCode ==REQUEST_CALL)
+        {
+            if (grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+            {
+                   MakePhoneCall(number);
+            }
+            else
+            {
+                Toast.makeText(getActivity(),"Permission fault",Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
 }
