430a03dad1faf5974a2f9434e8c4f824faf42fb7
abotarek5530 <abotarek19985530575@gmail.com>
2020-03-31 03:25:21 +0200

    AHMED TAREK 31/3/2020........... END THE TASK OF HOW CAN WE MAKE THE TEXTVIEW(GO TO ADDCAR ) GO TO THE ACTIVITY OF ADD CAR ALSO MAKE THE IMAGEVIEW (ARROW ) DO SAME FUNCTION FROM FRAGMENT TO ADDCAR ACTIVITY............

--- app/src/main/java/com/badawy/carservice/fragment/NavSettingsFragment.java
+++ app/src/main/java/com/badawy/carservice/fragment/NavSettingsFragment.java
@@ -1,6 +1,7 @@
 package com.badawy.carservice.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,9 +12,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.badawy.carservice.R;
+import com.badawy.carservice.activity.AddCarActivity;
+import com.badawy.carservice.activity.ForgotPasswordActivity;
 import com.badawy.carservice.activity.HomepageActivity;
+import com.badawy.carservice.activity.LoginActivity;
 import com.badawy.carservice.adapters.AccountInfoLabelsAdapter;
 import com.badawy.carservice.models.AccountInfoModel;
 
@@ -33,12 +38,25 @@ public class NavSettingsFragment extends Fragment {
         // Required empty public constructor
     }
 
+//VAR FOR TO GO TO THE ADDCAR ACTIVITY
+    private TextView gotoaddcar;
+    private ImageView gotoaddcar1;
+
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_nav_settings, container, false);
+//AHMED TAREK..................................
+        gotoaddcar=(TextView) view.findViewById(R.id.settings_addNewCarLabel);
+        gotoaddcar1=(ImageView)view.findViewById(R.id.settings_addNewCar);
+
+
+
+
+
         ImageView navMenuBtn = view.findViewById(R.id.settings_navMenuBtn);
         accountInfoRV= view.findViewById(R.id.settings_accountInfoRV);
 
@@ -53,6 +71,32 @@ public class NavSettingsFragment extends Fragment {
                 HomepageActivity.openDrawer();
             }
         });
+
+
+        //AHMED TAREK SAAD THIS FUNCTION FOR OR TO GO TO THE ADDCAR ACTIVITY BY CLICK ON THE (ADD NEW CAR) 31/3/2020.............
+
+      gotoaddcar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent = new Intent(getActivity(), AddCarActivity.class);
+                startActivity(myintent);
+
+            }
+        });
+        //AHMED TAREK SAAD THIS FUNCTION FOR OR TO GO TO THE ADDCAR ACTIVITY BY CLICK ON THE (image view (arrow)) 31/3/2020..............
+      gotoaddcar1.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              Intent myintent1=new Intent(getActivity(),AddCarActivity.class);
+              startActivity(myintent1);
+          }
+      });
+
+
+
+
+
+
 
         return view;
     }
