67b0387a0096ea9fbc759665ceee837bc39a8fb9
Mahmoud Badawy <m.badawy2011@gmail.com>
2020-04-13 17:27:38 +0200

    emergencyTracking UI

---
+++ app/src/main/java/com/badawy/carservice/fragment/EmergencyRequestTruckFragment.java
@@ -0,0 +1,29 @@
+package com.badawy.carservice.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.badawy.carservice.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EmergencyRequestTruckFragment extends Fragment {
+
+    public EmergencyRequestTruckFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_emergency_request_truck, container, false);
+    }
+}
